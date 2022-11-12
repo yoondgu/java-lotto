049d9305aa2cf335c1fe6f263e0d7218ebd17d43
yoondgu <doyou2ing@naver.com>
2022-11-13 01:20:55 +0900

    :sparkles:feat: 랜덤으로 로또 번호 생성하는 기능 구현 #1
    - 별도 단위테스트를 필요로 하지 않는 기능으로 판단함.
    - 추후 입출력 기능 구현 후 ApplicationTest 통과 필요.

--- src/main/java/lotto/domain/LottoSeller.java
+++ src/main/java/lotto/domain/LottoSeller.java
@@ -1,4 +1,14 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.constants.LottoRule;
+
+import java.util.List;
+
 public class LottoSeller {
+    private List<Integer> generateLottoNumbersByRandom() {
+        int startInclusive = LottoRule.MINIMUM_NUMBER.getValue();
+        int endInclusive = LottoRule.MAXIMUM_NUMBER.getValue();
+        return Randoms.pickUniqueNumbersInRange(startInclusive, endInclusive,6);
+    }
 }
