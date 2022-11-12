54c7b5bde89e394ab66ad763857790b7f98d1ea4
yoondgu <doyou2ing@naver.com>
2022-11-12 23:32:51 +0900

    :sparkles:feat: 로또 1장 발행하기 기능 및 관련 예외처리 구현 #2
    - Lotto 인스턴스 생성을 로또 1장 발행하는 것으로 정의함.
    - NumbersValidator는 로또 번호를 검증하여 예외발생시키는 정적 메소드를 제공함.
    - 1번 기능의 예외처리 사항을 2번 기능에서 처리하도록 변경, 기능목록에 반영함.
    - LottoTest의 예외 발생 테스트 3개 모두 통과.
    - 단위 테스트 목록에 현재 통과한 테스트를 반영함.

--- src/main/java/lotto/domain/validator/NumbersValidator.java
+++ src/main/java/lotto/domain/validator/NumbersValidator.java
@@ -1,4 +1,41 @@
 package lotto.domain.validator;
 
+import java.util.List;
+
 public class NumbersValidator {
+    public static void validateLottoNumbers(List<Integer> lottoNumbers) {
+        // TODO 각 예외사항에 적절한 에러메시지 Message 클래스에서 가져와 사용하기
+        if (hasInvalidSize(lottoNumbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호 숫자의 개수가 적절하지 않습니다.");
+        }
+        if (containsOutOfBoundsNumber(lottoNumbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호 숫자의 범위가 적절하지 않습니다.");
+        }
+        if (containsDuplicatedNumber(lottoNumbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복된 숫자를 가질 수 없습니다.");
+        }
+    }
+
+    private static boolean hasInvalidSize(List<Integer> lottoNumbers) {
+        // TODO 번호 개수 6 NumberRule 클래스에 저장하기
+        if (lottoNumbers.size() != 6) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean containsOutOfBoundsNumber(List<Integer> lottoNumbers) {
+        // TODO 최솟값 최댓값 NumberRule 클래스에 저장하기
+        return lottoNumbers.stream()
+                .anyMatch(number -> number < 1 || number > 45);
+    }
+
+    private static boolean containsDuplicatedNumber(List<Integer> lottoNumbers) {
+        int distinctedNumbersCount = (int) lottoNumbers.stream().distinct().count();
+        // TODO 번호 개수 6 대신 NumberRule 클래스의 값 가져와 사용하기
+        if (distinctedNumbersCount != 6) {
+            return true;
+        }
+        return false;
+    }
 }
