b2728e7842e8cc133cab605b17ff7595b2420673
yoondgu <doyou2ing@naver.com>
2022-11-15 04:39:08 +0900

    :art:style: 단위테스트 DisplayName 문장 형식 통일

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -9,28 +9,28 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 class LottoTest {
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생")
     @Test
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생")
     @Test
     void createLottoByDuplicatedNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호에 범위 밖의 숫자가 있으면 예외가 발생한다.")
+    @DisplayName("로또 번호에 범위 밖의 숫자가 있으면 예외가 발생")
     @Test
     void createLottoByOutOfBoundsNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 50)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("두 로또 번호를 비교하여 일치하는 수의 개수 반환 테스트")
+    @DisplayName("두 로또 번호를 비교하여 일치하는 수의 개수 반환")
     @Test
     void countMatchingNumbers() {
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
